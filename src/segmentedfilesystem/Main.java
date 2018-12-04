2eba58969465b1c88b0396e282eab8062f51ae95
Mark Schauer <marksthe1@gmail.com>
2018-12-04 01:36:57 -0600

    Add preliminary variables to store file metadata; add loop logic and helper functions

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -30,6 +30,79 @@ public class Main {
             byte[] receivedArray = new byte[received.getLength()];
 
             System.arraycopy(received.getData(), 0, receivedArray, 0, receivedArray.length);
+
+            byte status = receivedArray[0];
+            if (status % 2 == 0) {
+                header.add(receivedArray);
+            } else if (status % 4 == 3) {
+                int packetNumber = assignNumber(receivedArray[2], receivedArray[3]);
+                total += packetNumber + 1;
+                packets++;
+                data.add(receivedArray);
+                numPackets.add(receivedArray);
+            } else {
+                data.add(receivedArray);
+            }
+        }
+
+        byte fileOneID = header.get(0)[1];
+        byte fileTwoID = header.get(1)[1];
+        byte fileThreeID = header.get(2)[1];
+
+        int fileOneSize = getPackListSize(numPackets, fileOneID);
+        int fileTwoSize = getPackListSize(numPackets, fileTwoID);
+        int fileThreeSize = getPackListSize(numPackets, fileThreeID);
+
+        ArrayList<byte[]> fileOne = createList(data, fileOneSize, fileOneID);
+        ArrayList<byte[]> fileTwo = createList(data, fileTwoSize, fileTwoID);
+        ArrayList<byte[]> fileThree = createList(data, fileThreeSize, fileThreeID);
+
+
+        String fileOneName = getName(header, 0);
+        String fileTwoName = getName(header, 1);
+        String fileThreeName = getName(header, 2);
+
+        write(fileOne, fileOneName);
+        write(fileTwo, fileTwoName);
+        write(fileThree, fileThreeName);
+        socket.close();
+    }
+
+    public static int assignNumber(byte a, byte b) {
+        int num;
+        int newPacket;
+        int newerPacket;
+
+        if (a < 0) {
+            newPacket = a + 256;
+        } else {
+            newPacket = a;
+        }
+        
+        if (b < 0) {
+            newerPacket = b + 256;
+        } else {
+            newerPacket = b;
+        }
+        num = 256 * newPacket + newerPacket;
+        return num;
+    }
+
+    public static void sortPacketList(byte[] received, ArrayList<byte[]> header, ArrayList<byte[]> data) {
+        if (received[0] % 2 == 0) {
+            header.add(received);
+        } else {
+            data.add(received);
+        }
+    }
+
+    public static int getPackListSize(ArrayList<byte[]> numPacks, byte fileID) {
+        int initial = -1;
+        for (int i = 0; i < numPacks.size(); i++) {
+            byte[] array = numPacks.get(i);
+            if (array[1] == fileID) {
+                initial = assignNumber(array[2], array[3]) + 1;
+            }
         }
     }
 }
