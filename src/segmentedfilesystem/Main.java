3acd74f696a0f4d5c66b7fffa1d25bb2def32611
Mark Schauer <marksthe1@gmail.com>
2018-12-04 01:52:40 -0600

    Finish up logic; errors are present and must be fixed

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,5 +1,6 @@
 package segmentedfilesystem;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.*;
 import java.util.ArrayList;
@@ -49,13 +50,13 @@ public class Main {
         byte fileTwoID = header.get(1)[1];
         byte fileThreeID = header.get(2)[1];
 
-        int fileOneSize = getPackListSize(numPackets, fileOneID);
-        int fileTwoSize = getPackListSize(numPackets, fileTwoID);
-        int fileThreeSize = getPackListSize(numPackets, fileThreeID);
+        int fileOneSize = getSize(numPackets, fileOneID);
+        int fileTwoSize = getSize(numPackets, fileTwoID);
+        int fileThreeSize = getSize(numPackets, fileThreeID);
 
-        ArrayList<byte[]> fileOne = createList(data, fileOneSize, fileOneID);
-        ArrayList<byte[]> fileTwo = createList(data, fileTwoSize, fileTwoID);
-        ArrayList<byte[]> fileThree = createList(data, fileThreeSize, fileThreeID);
+        ArrayList<byte[]> fileOne = genList(data, fileOneSize, fileOneID);
+        ArrayList<byte[]> fileTwo = genList(data, fileTwoSize, fileTwoID);
+        ArrayList<byte[]> fileThree = genList(data, fileThreeSize, fileThreeID);
 
 
         String fileOneName = getName(header, 0);
@@ -78,7 +79,7 @@ public class Main {
         } else {
             newPacket = a;
         }
-        
+
         if (b < 0) {
             newerPacket = b + 256;
         } else {
@@ -88,15 +89,7 @@ public class Main {
         return num;
     }
 
-    public static void sortPacketList(byte[] received, ArrayList<byte[]> header, ArrayList<byte[]> data) {
-        if (received[0] % 2 == 0) {
-            header.add(received);
-        } else {
-            data.add(received);
-        }
-    }
-
-    public static int getPackListSize(ArrayList<byte[]> numPacks, byte fileID) {
+    public static int getSize(ArrayList<byte[]> numPacks, byte fileID) {
         int initial = -1;
         for (int i = 0; i < numPacks.size(); i++) {
             byte[] array = numPacks.get(i);
@@ -104,5 +97,45 @@ public class Main {
                 initial = assignNumber(array[2], array[3]) + 1;
             }
         }
+        return initial;
+    }
+
+    public static ArrayList<byte[]> genList(ArrayList<byte[]> data, int fileSize, byte fileID) {
+        ArrayList<byte[]> l = new ArrayList<byte[]>(fileSize);
+        for (int i = 0; i < fileSize; i++) {
+            l.add(null);
+        }
+
+        for (int i = 0; i < data.size(); i++) {
+            byte dataID = data.get(i)[1];
+            int num = assignNumber(data.get(i)[2], data.get(i)[3]);
+
+            if (dataID == fileID) {
+                l.set(num, data.get(i));
+            }
+        }
+        return l;
+    }
+
+    public static String getName(ArrayList<byte[]> header, int fileNum) {
+        String name = null;
+        byte[] byteName = new byte[header.get(fileNum).length - 2];
+
+        for (int i = 0; i < byteName.length; i++) {
+            byteName[i] = header.get(fileNum)[i + 2];
+        }
+        name = new String(byteName);
+        return name;
+    }
+
+    public static void write(ArrayList<byte[]> list, String fileName) throws IOException {
+        FileOutputStream file = new FileOutputStream(fileName);
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 4; j < list.get(i).length; j++) {
+                file.write(list.get(i)[j]);
+            }
+        }
+        file.close();
     }
 }
